ad87b72050424f612ee93ceacdff68faa2cd1df0
Damienvd <70859110+Damienvd@users.noreply.github.com>
2021-12-02 21:24:47 +0200

    github api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+
+(() => {
+  try {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var gitRepos = JSON.parse(this.responseText);
+           
+          const gitRepoParent = document.getElementById('git-repos');
+          
+          gitRepos.forEach(repo => {
+            const repoNodeContainer = document.createElement("div");
+            const repoNode = document.createElement("a");
+            repoNodeContainer.appendChild(repoNode);
+            repoNode.innerText = repo.name;
+            repoNode.setAttribute("href", repo.html_url);
+
+            gitRepoParent.appendChild(repoNodeContainer);
+          });
+
+        }
+    };
+    
+    xhttp.open("GET", "https://api.github.com/user/repos", true);
+    xhttp.setRequestHeader("Authorization", "token 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");
+    xhttp.send();
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+
+
 /*COPYRIGHT YEAR*/
 var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
